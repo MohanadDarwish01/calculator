c017860ed4aec64eab78417538ee6e9795a3dbaf
MohanadDarwish01 <mohanaddarwish01@gmail.com>
2025-01-21 15:36:23 +0200

    firsy commit

--- calculator.js
+++ calculator.js
@@ -1,28 +1,24 @@
 // calculate
 
 // addition
-let collectNumbers = (x, y) => {
-    return eval(x + y) ;
-}
+
 let collect2No = () => {
     let no1 = +prompt("Enter the first NO");
     let no2 = +prompt("Enter the Second NO");
-    let final = collectNumbers(no1, no2);
-    document.getElementById("result").innerHTML = final;
+    let final = no1 + "+" + no2;
+    document.getElementById("result").innerHTML = eval(final);
     document.getElementById("notice").innerHTML = " ";
 
 }
 
 
 // subtraction
-let subtractNumbers = (x, y) => {
-    return eval(x - y) ;
-}
+
 let subtract2No = () => {
     let no1 = +prompt("Enter the first NO");
     let no2 = +prompt("Enter the Second NO");
-    let final = subtractNumbers(no1, no2);
-    document.getElementById("result").innerHTML = final;
+    let final = no1 + "-" + no2;
+    document.getElementById("result").innerHTML = eval(final);
     document.getElementById("notice").innerHTML = " ";
     if(no1 < no2){
         document.getElementById("notice").innerHTML = "The number is negative";
@@ -32,14 +28,12 @@ let subtract2No = () => {
 
 
 // multiply
-let multiplyNumbers = (x, y) => {
-    return eval(x * y) ;
-}
+
 let multiply2No = () => {
     let no1 = +prompt("Enter the first NO");
     let no2 = +prompt("Enter the Second NO");
-    let final = multiplyNumbers(no1, no2);
-    document.getElementById("result").innerHTML = final;
+    let final = no1 + "*" + no2;
+    document.getElementById("result").innerHTML = eval(final);
     document.getElementById("notice").innerHTML = " ";
 
 }
@@ -48,16 +42,14 @@ let multiply2No = () => {
 
 
 // divide
-let divideNumbers = (x, y) => {
-    return eval(x / y) ;
-}
+
 let divide2No = () => {
     let no1 = +prompt("Enter the first NO");
     let no2 = +prompt("Enter the Second NO");
-    let final = divideNumbers(no1, no2);
-    document.getElementById("result").innerHTML = final;
+    let final = no1 + "/" + no2;
+    document.getElementById("result").innerHTML = eval(final);
     document.getElementById("notice").innerHTML = " ";
-    if(Math.ceil(final) != final){
+    if(Math.ceil(eval(final)) != eval(final)){
         document.getElementById("notice").innerHTML = "This is a number containing fractures";
     }
 }
